a747d20656eefcaa2280c1bed3724237c6cd0862
Paloma Wink <paloma.wink@hotmail.com>
2023-10-02 19:57:25 -0300

    Criacão da primeira rota

--- app/backend/src/controller/teams.controller.ts
+++ app/backend/src/controller/teams.controller.ts
@@ -1,13 +1,12 @@
+import { Request, Response } from 'express';
 import TeamsService from '../services/teams.service';
-import { ServiceResponse } from '../Interfaces/serviceResponse';
-import ITeams from '../Interfaces/Teams/Teams';
 
 export default class TeamsController {
   constructor(
     private _teamsService = new TeamsService(),
   ) {}
 
-  public async getAllTeams(_req: Request, res: Response): Promise<ServiceResponse<ITeams[]>> {
+  public async getAllTeams(_req: Request, res: Response) {
     const { status, data } = await this._teamsService.getAllTeams();
     return res.status(status).json(data);
   }
