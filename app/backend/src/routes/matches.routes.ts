489ec7c16259c115d3238e4e6dcd7bc1675a29ea
Paloma Wink <paloma.wink@hotmail.com>
2023-10-05 17:23:18 -0300

    Requisito 15 completo

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,13 +1,11 @@
 import { Router, Request, Response } from 'express';
 import MatchesController from '../controller/matches.controller';
-import Validations from '../middleware/Validations';
 
 const router = Router();
 const matchesController = new MatchesController();
 
 router.get(
   '/',
-  Validations.validateRegister,
   (req: Request, res: Response) => matchesController.getAllMatches(req, res),
 );
 // router.get('/:id', (req: Request, res: Response) => matchesController.getTeamById(req, res));
