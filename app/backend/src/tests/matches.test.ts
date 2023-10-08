aedbcbc19fc0be5e8b96aa64854ba96b2035a8c4
Paloma Wink <paloma.wink@hotmail.com>
2023-10-08 15:21:35 -0300

    Testes realizados para /matches para aumentar a cobertura

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -8,7 +9,9 @@ import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
 import Matches from '../database/models/Matches.Model';
-import { allMatches } from './mocks/matches.mock';
+import MatchesService from '../services/matches.service';
+import Validations from '../middleware/Validations';
+import { allMatches, finishedMatch, matchInProgress } from './mocks/matches.mock';
 
 chai.use(chaiHttp);
 
@@ -22,6 +25,28 @@ describe('Testando a rota /matches', () => {
 
     expect(status).to.equal(200);
     expect(body).to.deep.equal(allMatches);
+  });
+  it('Testando se e possivel pegar uma partida em andamento', async function () {
+    sinon.stub(Matches, 'findAll').resolves(allMatches as unknown as Matches[]);
+    sinon.stub(MatchesService.prototype, 'getAllMatches').resolves({status: 200, data: matchInProgress});
+    
+
+    const {status, body} = await chai
+      .request(app).get('/matches?inProgress=true')
+    
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchInProgress);
+  });
+  it('Testando se e possivel pegar uma partida ja finalizada', async function () {
+    sinon.stub(Matches, 'findAll').resolves(allMatches as unknown as Matches[]);
+    sinon.stub(MatchesService.prototype, 'getAllMatches').resolves({status: 200, data: finishedMatch});
+    
+
+    const {status, body} = await chai
+      .request(app).get('/matches?inProgress=false')
+    
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(finishedMatch);
   });
   afterEach(function () {
     sinon.restore();
--- app/backend/src/tests/mocks/matches.mock.ts
+++ app/backend/src/tests/mocks/matches.mock.ts
@@ -5,7 +5,7 @@ const allMatches = [
 		"homeTeamGoals": 1,
 		"awayTeamId": 8,
 		"awayTeamGoals": 1,
-		"inProgress": false,
+		"inProgress": true,
 		"homeTeam": {
 			"teamName": "São Paulo"
 		},
@@ -57,6 +57,68 @@ const allMatches = [
 	}
 ]
 
+const matchInProgress = [{
+	"id": 1,
+	"homeTeamId": 16,
+	"homeTeamGoals": 1,
+	"awayTeamId": 8,
+	"awayTeamGoals": 1,
+	"inProgress": true,
+	"homeTeam": {
+		"teamName": "São Paulo"
+	},
+	"awayTeam": {
+		"teamName": "Grêmio"
+	}
+}]
+
+const finishedMatch = [
+	{
+		"id": 2,
+		"homeTeamId": 9,
+		"homeTeamGoals": 1,
+		"awayTeamId": 14,
+		"awayTeamGoals": 1,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Internacional"
+		},
+		"awayTeam": {
+			"teamName": "Santos"
+		}
+	},
+	{
+		"id": 3,
+		"homeTeamId": 4,
+		"homeTeamGoals": 3,
+		"awayTeamId": 11,
+		"awayTeamGoals": 0,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Corinthians"
+		},
+		"awayTeam": {
+			"teamName": "Napoli-SC"
+		}
+	},
+	{
+		"id": 4,
+		"homeTeamId": 3,
+		"homeTeamGoals": 0,
+		"awayTeamId": 2,
+		"awayTeamGoals": 0,
+		"inProgress": false,
+		"homeTeam": {
+			"teamName": "Botafogo"
+		},
+		"awayTeam": {
+			"teamName": "Bahia"
+		}
+	}
+]
+
 export {
 	allMatches,
+	finishedMatch,
+	matchInProgress
 };
