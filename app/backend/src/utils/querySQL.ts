bed243b9dcfcdc1b90cc964ccb77908500778cc8
Paloma Wink <paloma.wink@hotmail.com>
2023-10-10 18:11:59 -0300

    Querys criadas para os endpoints /, /home e /away

--- app/backend/src/utils/querySQL.ts
+++ app/backend/src/utils/querySQL.ts
@@ -15,7 +15,7 @@ INNER JOIN
 teams AT ON M.away_team_id = AT.id
 WHERE M.in_progress =`;
 
-const leaderboardQuery = `SELECT
+const leaderboardQueryAllTeams = `SELECT
 t.team_name AS name,
 SUM(
     CASE
@@ -26,27 +26,30 @@ SUM(
         ELSE 0
     END
 ) AS totalPoints,
-COUNT(
+SUM(
     CASE
         WHEN m.home_team_id = t.id OR m.away_team_id = t.id THEN 1
     END
 ) AS totalGames,
-COUNT(
+SUM(
     CASE
         WHEN m.home_team_id = t.id AND m.home_team_goals > m.away_team_goals THEN 1
         WHEN m.away_team_id = t.id AND m.away_team_goals > m.home_team_goals THEN 1
+        ELSE 0
     END
 ) AS totalVictories,
-COUNT(
+SUM(
     CASE
         WHEN m.home_team_id = t.id AND m.home_team_goals = m.away_team_goals THEN 1
         WHEN m.away_team_id = t.id AND m.away_team_goals = m.home_team_goals THEN 1
+        ELSE 0
     END
 ) AS totalDraws,
-COUNT(
+SUM(
     CASE
         WHEN m.home_team_id = t.id AND m.home_team_goals < m.away_team_goals THEN 1
         WHEN m.away_team_id = t.id AND m.away_team_goals < m.home_team_goals THEN 1
+        ELSE 0
     END
 ) AS totalLosses,
 SUM(
@@ -79,14 +82,155 @@ ROUND((SUM(
     CASE
         WHEN m.home_team_id = t.id OR m.away_team_id = t.id THEN 1
     END
-)) * 100), 2) AS efficiency
+)* 3) * 100), 2) AS efficiency
 FROM
 teams AS t
 INNER JOIN
 matches AS m ON t.id = m.home_team_id OR t.id = m.away_team_id
+WHERE m.in_progress = 0
 GROUP BY
 t.id, t.team_name
 ORDER BY
-totalPoints DESC, goalsBalance DESC, goalsFavor DESC`;
+totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC`;
 
-export { inProgressQuery, leaderboardQuery };
+const leaderboardQueryHomeTeams = `SELECT
+t.team_name AS name,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id AND m.home_team_goals > m.away_team_goals THEN 3
+        WHEN m.home_team_id = t.id AND m.home_team_goals = m.away_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalPoints,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id THEN 1
+    END
+) AS totalGames,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id AND m.home_team_goals > m.away_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalVictories,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id AND m.home_team_goals = m.away_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalDraws,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id AND m.home_team_goals < m.away_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalLosses,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id THEN m.home_team_goals
+    END
+) AS goalsFavor,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id THEN m.away_team_goals
+    END
+) AS goalsOwn,
+SUM(
+    CASE
+        WHEN m.home_team_id = t.id THEN m.home_team_goals - m.away_team_goals
+    END
+) AS goalsBalance,
+ROUND((SUM(
+    CASE
+        WHEN m.home_team_id = t.id AND m.home_team_goals > m.away_team_goals THEN 3
+        WHEN m.home_team_id = t.id AND m.home_team_goals = m.away_team_goals THEN 1
+        ELSE 0
+    END
+) / (COUNT(
+    CASE
+        WHEN m.home_team_id = t.id THEN 1
+    END
+)* 3) * 100), 2) AS efficiency
+FROM
+teams AS t
+INNER JOIN
+matches AS m ON t.id = m.home_team_id
+WHERE m.in_progress = 0
+GROUP BY
+t.id, t.team_name
+ORDER BY
+totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC`;
+
+const leaderboardQueryAwayTeams = `SELECT
+t.team_name AS name,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id AND m.away_team_goals > m.home_team_goals THEN 3
+        WHEN m.away_team_id = t.id AND m.away_team_goals = m.home_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalPoints,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id THEN 1
+    END
+) AS totalGames,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id AND m.away_team_goals > m.home_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalVictories,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id AND m.away_team_goals = m.home_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalDraws,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id AND m.away_team_goals < m.home_team_goals THEN 1
+        ELSE 0
+    END
+) AS totalLosses,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id THEN m.away_team_goals
+    END
+) AS goalsFavor,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id THEN m.home_team_goals
+    END
+) AS goalsOwn,
+SUM(
+    CASE
+        WHEN m.away_team_id = t.id THEN m.away_team_goals - m.home_team_goals
+    END
+) AS goalsBalance,
+ROUND((SUM(
+    CASE
+        WHEN m.away_team_id = t.id AND m.away_team_goals > m.home_team_goals THEN 3
+        WHEN m.away_team_id = t.id AND m.away_team_goals = m.home_team_goals THEN 1
+        ELSE 0
+    END
+) / (COUNT(
+    CASE
+        WHEN m.away_team_id = t.id THEN 1
+    END
+)* 3) * 100), 2) AS efficiency
+FROM
+teams AS t
+INNER JOIN
+matches AS m ON t.id = m.away_team_id
+WHERE m.in_progress = 0
+GROUP BY
+t.id, t.team_name
+ORDER BY
+totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC`;
+
+export { inProgressQuery,
+  leaderboardQueryAllTeams,
+  leaderboardQueryHomeTeams,
+  leaderboardQueryAwayTeams,
+};
