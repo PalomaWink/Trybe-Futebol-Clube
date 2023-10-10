a307c1003752cdab76b3f5dadc13d334c106536c
Paloma Wink <paloma.wink@hotmail.com>
2023-10-10 18:08:52 -0300

    Controller para os requisitos 23 até o 30

--- app/backend/src/controller/leaderboard.controller.ts
+++ app/backend/src/controller/leaderboard.controller.ts
@@ -8,8 +8,18 @@ export default class LeaderboardController {
 
   }
 
-  public async getLeaderboard(_req: Request, res: Response): Promise<Response> {
-    const { status, data } = await this._leaderboardService.getLeaderboard();
+  public async getAllLeaderboard(_req: Request, res: Response): Promise<Response> {
+    const { status, data } = await this._leaderboardService.getAllLeaderboard();
+    return res.status(status).json(data);
+  }
+
+  public async getHomeLeaderboard(_req: Request, res: Response): Promise<Response> {
+    const { status, data } = await this._leaderboardService.getHomeLeaderboard();
+    return res.status(status).json(data);
+  }
+
+  public async getAwayLeaderboard(_req: Request, res: Response): Promise<Response> {
+    const { status, data } = await this._leaderboardService.getAwayLeaderboard();
     return res.status(status).json(data);
   }
 }
