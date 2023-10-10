83f69f7e9cc46721b4b8d9ea5c6d95badee0845d
Paloma Wink <paloma.wink@hotmail.com>
2023-10-10 18:10:18 -0300

    Rotas leaderboard /, /home e /away

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -5,8 +5,16 @@ const router = Router();
 const leaderboardController = new LeaderboardController();
 
 router.get(
+  '/',
+  (req: Request, res: Response) => leaderboardController.getAllLeaderboard(req, res),
+);
+router.get(
   '/home',
-  (req: Request, res: Response) => leaderboardController.getLeaderboard(req, res),
+  (req: Request, res: Response) => leaderboardController.getHomeLeaderboard(req, res),
+);
+router.get(
+  '/away',
+  (req: Request, res: Response) => leaderboardController.getAwayLeaderboard(req, res),
 );
 
 export default router;
