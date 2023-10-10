aa2d05b0471bb37db9e8a2dcdc611c28bcbcc263
Paloma Wink <paloma.wink@hotmail.com>
2023-10-10 18:11:30 -0300

    Service criada para os requisitos 23 até o 30

--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -1,5 +1,7 @@
 import TeamRanking from '../Interfaces/Leaderboard/Leaderboard';
-import { leaderboardQuery } from '../utils/querySQL';
+import { leaderboardQueryAllTeams,
+  leaderboardQueryHomeTeams,
+  leaderboardQueryAwayTeams } from '../utils/querySQL';
 import sequelize from '../database/models/index';
 
 export default class LeaderboardService {
@@ -7,8 +9,21 @@ export default class LeaderboardService {
     private _sequelize = sequelize,
   ) {}
 
-  public async getLeaderboard() {
-    const [query] = await this._sequelize.query(leaderboardQuery) as unknown as TeamRanking[];
+  public async getAllLeaderboard() {
+    const [query] = await this._sequelize
+      .query(leaderboardQueryAllTeams) as unknown as TeamRanking[];
+    return { status: 200, data: query };
+  }
+
+  public async getHomeLeaderboard() {
+    const [query] = await this._sequelize
+      .query(leaderboardQueryHomeTeams) as unknown as TeamRanking[];
+    return { status: 200, data: query };
+  }
+
+  public async getAwayLeaderboard() {
+    const [query] = await this._sequelize
+      .query(leaderboardQueryAwayTeams) as unknown as TeamRanking[];
     return { status: 200, data: query };
   }
 }
